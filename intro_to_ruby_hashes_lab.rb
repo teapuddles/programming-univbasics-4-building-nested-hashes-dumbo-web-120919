2e41382d6e7b3463b9cb9585ee91a00a7d249fc1
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-29 23:12:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,8 @@ end
 
 def monopoly_with_third_tier
   monopoly = {
-     :railroads => {pieces: "4", rent_in_dollars: ""
+     :railroads => {pieces: "4",
+     :rent_in_dollars => ""
   }
   }
 end
